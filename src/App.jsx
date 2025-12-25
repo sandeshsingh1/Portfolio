8808af824f4988ab410d313c41f1d9e783bf5c80
Sandesh SIngh <sandeshsingh201106@gmail.com>
2025-12-25 22:53:48 +0530

    Added Services Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from './Components/Navbar/Navbar'
 import { Hero } from './Components/Hero/Hero'
 import { About } from './Components/About/About'
+import { Services } from './Components/Services/Services'
 
 export const App = () => {
   return (
@@ -9,6 +10,7 @@ export const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services/>
     </div>
   )
 }
